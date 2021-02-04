ae914754959e29cef73bb8222d7ac648bbb61c0b
gioboa <giorgiob.boa@gmail.com>
2021-02-04 12:35:43 +0100

    dev: added initial step files

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
+  import './App.css';
+
   export let title;
 </script>
 
 <main>
-  <h1>{title}</h1>
+  <div class="widget">
+    <div class="left-panel panel">
+      <div class="date">Monday, 20 Aug 2018</div>
+      <div class="city">
+        {title}
+      </div>
+      <div class="temp">
+        <!-- <img src="https://s5.postimg.cc/yzcm7htyb/image.png" alt="" width="60" /> -->
+        18&deg;
+      </div>
+    </div>
+    <div class="right-panel panel">
+      <img src="images/london.png" alt="" width="120" />
+    </div>
+  </div>
 </main>
-
-<style>
-</style>
